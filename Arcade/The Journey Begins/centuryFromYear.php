93499939670e9e6b120c7ccbc36b74683a3217ff
Md. Rakibul Islam <44586913+rakibul-dev@users.noreply.github.com>
2020-12-08 00:29:42 +0600

    Update centuryFromYear.php

--- Arcade/The Journey Begins/centuryFromYear.php
+++ Arcade/The Journey Begins/centuryFromYear.php
@@ -6,7 +6,8 @@ function centuryFromYear($year) {
 
 
 /*
-Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, 
+the second - from the year 101 up to and including the year 200, etc.
 
 Example
 
