9e609ea5d1fa988cac1f5010c7501286e876a5c7
Md. Rakibul Islam <44586913+rakibul-dev@users.noreply.github.com>
2020-12-10 01:03:28 +0600

    Create Mirror Bits.php

---
+++ Arcade/Corner of 0s and 1s/Mirror Bits.php
@@ -0,0 +1,20 @@
+<?php
+
+function mirrorBits($a) {
+    return bindec(strrev(decbin($a)));
+}
+
+/*
+Reverse the order of the bits in a given integer.
+
+Example
+
+For a = 97, the output should be
+mirrorBits(a) = 67.
+
+97 equals to 1100001 in binary, which is 1000011 after mirroring, and that is 67 in base 10.
+
+For a = 8, the output should be
+mirrorBits(a) = 1.
+
+*/
